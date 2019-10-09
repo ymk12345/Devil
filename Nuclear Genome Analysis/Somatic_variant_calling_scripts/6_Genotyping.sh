b130d963f0c42e2c894b57babad88f238268cb0d
ymk12345 <47306876+ymk12345@users.noreply.github.com>
2019-10-09 15:47:45 -0400

    Update 6_Genotyping.sh

--- Nuclear Genome Analysis/Somatic_variant_calling_scripts/6_Genotyping.sh
+++ Nuclear Genome Analysis/Somatic_variant_calling_scripts/6_Genotyping.sh
@@ -12,17 +12,17 @@ NAME=`basename $VCF`
 	       	--logFileName="${NAME%.*}"_default.log \
     		--refFile=$REFERENCE \
          	--bamFiles=$BAMLIST \
-            --minReads=1 \
-			--minFlank=0 \
-			--minBaseQual=30 \
+            	--minReads=1 \
+		--minFlank=0 \
+		--minBaseQual=30 \
       		--filteredReadsFrac=.8 \
-			--badReadsThreshold=30 \
+		--badReadsThreshold=30 \
 	        --badReadsWindow=15 \
-            --minPosterior=0 \
+            	--minPosterior=0 \
 	        --getVariantsFromBAMs=0 \
 	        --bufferSize=500 \
-			--regions=$REGIONS \
+		--regions=$REGIONS \
 	        --source=$VCF \
-            --nCPU=1 \
-            -o $OUTDIR/"${NAME%.*}".genotyped.vcf
+            	--nCPU=1 \
+            	-o $OUTDIR/"${NAME%.*}".genotyped.vcf
                     
